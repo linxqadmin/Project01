7a079fb0edcc49c9c677a9bbb461dc8c64e6983f
linxiaoqing <linxqjy@163.com>
2022-06-02 09:17:39 +0800

    Initial commit

---
+++ src/main/java/com/example/project01/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.project01;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
